98851f374e34fad7c0e36e10785f6caa2ca234b7
edwinyoon <ep160101@gmail.com>
2019-01-14 22:21:55 -0500

    Add BundleAnalyzerPlugin setup

--- webpack/webpack-boilerplate/webpack.config.prod.js
+++ webpack/webpack-boilerplate/webpack.config.prod.js
@@ -4,4 +4,12 @@ const baseConfig = require('./webpack.config.base.js');
 
 module.exports = merge(baseConfig, {
   mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      /** This plugin will give us a report about our app like bundle sizes and which libraries are making bundle size bigger, etc... */
+      analyzerMode: 'static',
+      // openAnalyzer: false,
+      reportFilename: 'bundle_sizes.html'
+    })
+  ]
 });
